5ff6f5892271740b4778f015f6648f17c9255730
clovon <channel.clovon@gmail.com>
2022-07-17 10:38:21 +0545

    Let's Build A Multi-Purpose Laravel 9 and Vue 3 Application | Part 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+
+import 'admin-lte/plugins/jquery/jquery.min.js';
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/dashboard', function () {
+    return view('dashboard');
+});
