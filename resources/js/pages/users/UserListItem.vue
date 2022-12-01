4c483103153739d005b271520261856fea4c91bb
clovon <channel.clovon@gmail.com>
2022-12-01 22:20:59 +0545

    Vue 3 Events naming convention and dispatching event from the template | Laravel & Vue 3 | Part 16

--- resources/js/pages/users/UserListItem.vue
+++ resources/js/pages/users/UserListItem.vue
@@ -28,10 +28,6 @@ const deleteUser = () => {
     });
 };
 
-const editUser = (user) => {
-    emit('editUser', user);
-};
-
 </script>
 <template>
     <tr>
@@ -41,7 +37,7 @@ const editUser = (user) => {
         <td>{{ formatDate(user.created_at) }}</td>
         <td>{{ user.role }}</td>
         <td>
-            <a href="#" @click.prevent="editUser(user)"><i class="fa fa-edit"></i></a>
+            <a href="#" @click.prevent="$emit('editUser', user)"><i class="fa fa-edit"></i></a>
             <a href="#" @click.prevent="confirmUserDeletion(user)"><i class="fa fa-trash text-danger ml-2"></i></a>
         </td>
     </tr>
