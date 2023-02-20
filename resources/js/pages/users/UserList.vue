d15fb00ef047c0169cde6e5130245f81eb70b16a
clovon <channel.clovon@gmail.com>
2023-02-20 09:20:35 +0545

    Fix Bug: Delete user is not working using  Vue 3 and Laravel | Part 27

--- resources/js/pages/users/UserList.vue
+++ resources/js/pages/users/UserList.vue
@@ -90,10 +90,6 @@ const handleSubmit = (values, actions) => {
     }
 }
 
-const userDeleted = (userId) => {
-    users.value = users.value.filter(user => user.id !== userId);
-};
-
 const searchQuery = ref(null);
 
 const search = () => {
@@ -119,6 +115,21 @@ const toggleSelection = (user) => {
         selectedUsers.value.splice(index, 1);
     }
     console.log(selectedUsers.value);
+};
+
+const userIdBeingDeleted = ref(null);
+const confirmUserDeletion = (id) => {
+    userIdBeingDeleted.value = id;
+    $('#deleteUserModal').modal('show');
+};
+
+const deleteUser = () => {
+    axios.delete(`/api/users/${userIdBeingDeleted.value}`)
+    .then(() => {
+        $('#deleteUserModal').modal('hide');
+        toastr.success('User deleted successfully!');
+        users.value.data = users.value.data.filter(user => user.id !== userIdBeingDeleted.value);
+    });
 };
 
 const bulkDelete = () => {
@@ -207,8 +218,13 @@ onMounted(() => {
                             </tr>
                         </thead>
                         <tbody v-if="users.data.length > 0">
-                            <UserListItem v-for="(user, index) in users.data" :key="user.id" :user=user :index=index
-                                @edit-user="editUser" @user-deleted="userDeleted" @toggle-selection="toggleSelection" :select-all="selectAll" />
+                            <UserListItem v-for="(user, index) in users.data"
+                                :key="user.id"
+                                :user=user :index=index
+                                @edit-user="editUser"
+                                @confirm-user-deletion="confirmUserDeletion"
+                                @toggle-selection="toggleSelection"
+                                :select-all="selectAll" />
                         </tbody>
                         <tbody v-else>
                             <tr>
@@ -266,6 +282,29 @@ onMounted(() => {
                         <button type="submit" class="btn btn-primary">Save</button>
                     </div>
                 </Form>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="deleteUserModal" data-backdrop="static" tabindex="-1" role="dialog"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">
+                        <span>Delete User</span>
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h5>Are you sure you want to delete this user ?</h5>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button @click.prevent="deleteUser" type="button" class="btn btn-primary">Delete User</button>
+                </div>
             </div>
         </div>
     </div>
--- resources/js/pages/users/UserListItem.vue
+++ resources/js/pages/users/UserListItem.vue
@@ -12,23 +12,7 @@ const props = defineProps({
     selectAll: Boolean,
 });
 
-const emit = defineEmits(['userDeleted', 'editUser']);
-
-const userIdBeingDeleted = ref(null);
-
-const confirmUserDeletion = (user) => {
-    userIdBeingDeleted.value = user.id;
-    $('#deleteUserModal').modal('show');
-};
-
-const deleteUser = () => {
-    axios.delete(`/api/users/${userIdBeingDeleted.value}`)
-    .then(() => {
-        $('#deleteUserModal').modal('hide');
-        toastr.success('User deleted successfully!');
-        emit('userDeleted', userIdBeingDeleted.value);
-    });
-};
+const emit = defineEmits(['userDeleted', 'editUser', 'confirmUserDeletion']);
 
 const roles = ref([
     {
@@ -68,30 +52,7 @@ const toggleSelection = () => {
         </td>
         <td>
             <a href="#" @click.prevent="$emit('editUser', user)"><i class="fa fa-edit"></i></a>
-            <a href="#" @click.prevent="confirmUserDeletion(user)"><i class="fa fa-trash text-danger ml-2"></i></a>
+            <a href="#" @click.prevent="$emit('confirmUserDeletion', user.id)"><i class="fa fa-trash text-danger ml-2"></i></a>
         </td>
     </tr>
-
-    <div class="modal fade" id="deleteUserModal" data-backdrop="static" tabindex="-1" role="dialog"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">
-                        <span>Delete User</span>
-                    </h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <h5>Are you sure you want to delete this user ?</h5>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button @click.prevent="deleteUser" type="button" class="btn btn-primary">Delete User</button>
-                </div>
-            </div>
-        </div>
-    </div>
 </template>
