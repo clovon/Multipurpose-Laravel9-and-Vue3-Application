342ac02fe05cc9e6a3163776eb1b52f524e6eb42
clovon <channel.clovon@gmail.com>
2023-05-31 15:33:03 +0545

    add dashboard stats page design stub

---
+++ resources/views/stubs/dashboard-stats.blade.php
@@ -0,0 +1,52 @@
+<div class="row">
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-info">
+            <div class="inner">
+                <div class="d-flex justify-content-between">
+                    <h3>1</h3>
+                    <select
+                        style="height: 2rem; outline: 2px solid transparent;" class="px-1 rounded border-0">
+                        <option value="">All</option>
+                        <option value="scheduled">Scheduled</option>
+                        <option value="closed">Closed</option>
+                    </select>
+                </div>
+                <p>Appointments</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+                View Appointments
+                <i class="fas fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+        <div class="small-box bg-info">
+            <div class="inner">
+                <div class="d-flex justify-content-between">
+                    <h3>0</h3>
+                    <select
+                        style="height: 2rem; outline: 2px solid transparent;" class="px-1 rounded border-0">
+                        <option value="TODAY">Today</option>
+                        <option value="30">30 days</option>
+                        <option value="60">60 days</option>
+                        <option value="360">360 days</option>
+                        <option value="MTD">Month to Date</option>
+                        <option value="YTD">Year to Date</option>
+                    </select>
+                </div>
+                <p>Users</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+                View Users
+                <i class="fas fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div>
+</div>
