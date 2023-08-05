a524b5f6ff0db95f1118e8030eceb1373b63a04b
clovon <channel.clovon@gmail.com>
2023-08-05 13:18:17 +0545

    add favicon

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminLTE 3 | Starter</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="icon" type="image/png" sizes="16x16" href="https://public-files.gumroad.com/7cgkhx8y9f72akfe4iikrqhwkdxs">
 </head>
 
 <body class="hold-transition sidebar-mini">
